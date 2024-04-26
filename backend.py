24ddacfa366bd37a1f4af045a074df9ae549fb47
spinoandraptos <spinoandraptos@gmail.com>
2024-04-26 16:20:04 +0800

    Added frontend basic setup

--- backend.py
+++ backend.py
@@ -9,12 +9,8 @@ Prerequisites:
 4) Computer running this application is installed with the same ODBC driver used by the SQL Server
 
 '''
-
 import pyodbc
-import urllib.request
-import json
 import os
-import ssl
 import pandas as pd
 import threading
 
@@ -23,10 +19,6 @@ DATABASE = 'TracerES'
 USERNAME = 'sa'
 PASSWORD = 'tracerE$'
 UDP_PORT = '1433'
-ML_ENDPOINT = 'https://trane-ml-nijct.southeastasia.inference.ml.azure.com/score'
-# Replace this with the primary/secondary key or AMLToken for the endpoint
-API_KEY = 'jnDmwRxpcYpcfHfRqBMAe3dSu43SNcnP'
-CSV_LOG = './ML_log_updated.csv'
 
 PARAM_1_SQL_QUERY = """
 SELECT TOP 1 
@@ -83,12 +75,7 @@ ORDER BY
 LastFetchTime DESC;
 """
 
-def allowSelfSignedHttps(allowed):
-    # bypass the server certificate verification on client side
-    if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
-        ssl._create_default_https_context = ssl._create_unverified_context
-
-def get_ML_prediction():
+def get_latest_data():
 
   connectionString = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};PORT={UDP_PORT};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD};'
   conn = pyodbc.connect(connectionString) 
@@ -105,54 +92,10 @@ def get_ML_prediction():
   param_4 = df_4.loc[0,'DataLogValue'].astype(float)
   param_5 = df_5.loc[0,'DataLogValue'].astype(float)
 
-  allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
-
-  data =  {
-    "input_data": {
-      "columns": [
-        "CurrTime5Value",
-        "CurrTime4Value",
-        "CurrTime2Value",
-        "CurrTime1Value"
-      ],
-      "index": [1],
-      "data": [[param_5, param_4, param_2, param_1]]
-    }
-  }
-
-  body = str.encode(json.dumps(data))
-
-  # The azureml-model-deployment header will force the request to go to a specific deployment.
-  # Remove this header to have the request observe the endpoint traffic rules
-  headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ API_KEY), 'azureml-model-deployment': 'automl-trane-model-1' }
-  req = urllib.request.Request(ML_ENDPOINT, body, headers)
-
-  try:
-      response = urllib.request.urlopen(req)
-      result = response.read().decode()
-      result = result.strip("[").strip("]")
-
-      df = pd.read_csv(CSV_LOG)
-      if df.empty:
-        new_row = pd.DataFrame([[curr_time, result, 0]],columns=['Time','ML Endpoint Predicted Future', 'Actual Future Value'])
-        df = pd.concat([df,new_row], ignore_index = True)
-      else:
-        curr_last_index = df.iloc[-1].name
-        new_row = pd.DataFrame([[curr_time, result, 0]],columns=['Time','ML Endpoint Predicted Future', 'Actual Future Value'])
-        df = pd.concat([df,new_row], ignore_index = True)
-        df["Actual Future Value"][curr_last_index] = param_3
-      df = df.reset_index(drop=True)
-      df.to_csv(CSV_LOG, index=False)
-
-  except urllib.error.HTTPError as error:
-      print("The request failed with status code: " + str(error.code))
-      # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-      print(error.info())
-      print(error.read().decode("utf8", 'ignore'))
 
 def run_function():
     thread = threading.Timer(60.0, run_function) # 60 seconds = 1 minute
     thread.start()
-    get_ML_prediction()
+    get_latest_data()
 
 run_function() # start the timer
--- frontend.py
+++ frontend.py
@@ -1,5 +1,20 @@
 '''
-Front-end GUI components and their communication with the backend
+Front-end GUI components and their communication with the backend and ML model
 '''
 
-import backend as server
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+
+app = QApplication([])
+app.setStyle('Fusion')
+palette = QPalette()
+palette.setColor(QPalette.ButtonText, Qt.red)
+app.setPalette(palette)
+window = QWidget()
+layout = QVBoxLayout()
+layout.addWidget(QPushButton('Top'))
+layout.addWidget(QPushButton('Bottom'))
+window.setLayout(layout)
+window.show()
+app.exec()
